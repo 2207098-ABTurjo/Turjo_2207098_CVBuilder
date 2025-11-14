d376c371408de752371ca5302d6adf3dd2d66997
AB <turjo2207098@stud.kuet.ac.bd>
2025-11-15 01:36:14 +0600

    Initial Commit

---
+++ src/main/java/com/example/cvbuilder/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.cvbuilder;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void button(ActionEvent actionEvent) {
+
+    }
+}
